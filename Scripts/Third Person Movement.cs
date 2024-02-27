be2d1498a33956633756814b15d283a1f1466a97
Shane Walsh <83427120+ShanewalshGit@users.noreply.github.com>
2024-02-27 20:02:15 +0000

    Added Cinemachine camera variations

--- Scripts/Third Person Movement.cs
+++ Scripts/Third Person Movement.cs
@@ -13,6 +13,22 @@ public class ThirdPersonMovement : MonoBehaviour
 
     [SerializeField] public float rotationSpeed;
 
+    // Camera styles
+    public CamStyle styleInUse;
+    public Transform shooterLookAt;
+
+    // Cinemachine camera objects
+    public GameObject thirdPersonCam;
+    public GameObject topDownCam;
+    public GameObject shooterCam;
+
+    public enum CamStyle // Camera styles to be used in the game
+    {
+        Basic,
+        Shooter,
+        TopDown
+    }
+
 
 private void Start()
     {
@@ -21,17 +37,68 @@ private void Start()
     }
     private void Update()
     {
+        // Switch camera style
+        if (Input.GetKeyDown(KeyCode.Alpha1))
+        {
+            SwitchCam(CamStyle.Basic);
+        }
+        else if (Input.GetKeyDown(KeyCode.Alpha2))
+        {
+            SwitchCam(CamStyle.Shooter);
+        }
+        else if (Input.GetKeyDown(KeyCode.Alpha3))
+        {
+            SwitchCam(CamStyle.TopDown);
+        }
+
         Vector3 viewDir = player.position - new Vector3(transform.position.x, player.position.y, transform.position.z);
         orientDirection.forward = viewDir.normalized;
 
-        // rotate palyer object
-        float Horizontal = Input.GetAxis("Horizontal");
-        float Vertical = Input.GetAxis("Vertical");
-        Vector3 direction = orientDirection.forward * Vertical + orientDirection.right * Horizontal;
-
-        if(direction != Vector3.zero)
+        // rotate palyer object based on camera style in use
+        if (styleInUse == CamStyle.Basic || styleInUse == CamStyle.TopDown)
         {
-            playerObj.forward = Vector3.Slerp(playerObj.forward, direction.normalized, Time.deltaTime * rotationSpeed);
+            float Horizontal = Input.GetAxis("Horizontal");
+            float Vertical = Input.GetAxis("Vertical");
+            Vector3 direction = orientDirection.forward * Vertical + orientDirection.right * Horizontal;
+
+            if(direction != Vector3.zero)
+            {
+                playerObj.forward = Vector3.Slerp(playerObj.forward, direction.normalized, Time.deltaTime * rotationSpeed);
+            }
         }
+        else if (styleInUse == CamStyle.Shooter)
+        {
+            Vector3 dirToShooterLookAtObj = shooterLookAt.position - new Vector3(transform.position.x, shooterLookAt.position.y, transform.position.z);
+            orientDirection.forward = dirToShooterLookAtObj.normalized;
+
+            playerObj.forward = orientDirection.forward;
+        }
+        else if (styleInUse == CamStyle.TopDown)
+        {
+            
+        }
+        
+    }
+
+    private void SwitchCam(CamStyle style)
+    {
+        shooterCam.SetActive(false);
+        thirdPersonCam.SetActive(false);
+        topDownCam.SetActive(false);
+
+        if(style == CamStyle.Basic)
+        {
+            thirdPersonCam.SetActive(true);
+        }
+        else if(style == CamStyle.Shooter)
+        {
+            shooterCam.SetActive(true);
+        }
+        else if(style == CamStyle.TopDown)
+        {
+            topDownCam.SetActive(true);
+        }
+
+        styleInUse = style;
     }
 }
