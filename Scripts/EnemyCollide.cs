677bc6ba823b6ac0a3b24fd85c1b95be320c7633
Shane Walsh <83427120+ShanewalshGit@users.noreply.github.com>
2024-03-07 09:13:59 +0000

    Adjusted EnemyCollide.cs to detect a bullet collision / Player collision kills

--- Scripts/EnemyCollide.cs
+++ Scripts/EnemyCollide.cs
@@ -2,22 +2,33 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Scripting.APIUpdating;
+using UnityEngine.SceneManagement;
 
 public class EnemyCollide : MonoBehaviour
 {
     Rigidbody rb;
 
-    // Start is called before the first frame update
-    void Start()
+    private void OnCollisionEnter(Collision other)
     {
-        rb = GetComponent<Rigidbody>();
+        if (other.gameObject.tag == "Player")
+        {
+            Destroy(other.gameObject);
+            var currentSceneIndex = SceneManager.GetActiveScene().buildIndex;
+            SceneManager.LoadScene(currentSceneIndex);
+            FindAnyObjectByType<SessionController>().RemoveLife();
+        }
     }
-    private void OnCollisionEnter(Collision collision)
-    {
-        if (collision.gameObject.tag == "Player")
+
+    private void OnTriggerEnter(Collider other) {
         {
             Destroy(gameObject);
-            FindObjectOfType<SessionController>().AddPoints(10);
+
+            var bullet = other.gameObject.GetComponent<Bullet>();
+            if (bullet)
+            {
+                Destroy(bullet.gameObject);
+                FindAnyObjectByType<SessionController>().AddPoints(10);
+            }
         }
     }
 }
