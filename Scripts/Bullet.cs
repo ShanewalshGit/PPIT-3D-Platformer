107357df295ea567be1386b02b9d5cb3ebb3b7df
Shane Walsh <83427120+ShanewalshGit@users.noreply.github.com>
2024-03-07 09:13:09 +0000

    Created bullet script for bullet prefab, largely handles hit interactions

---
+++ Scripts/Bullet.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bullet : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        //Destroy the bullet after 2 seconds
+        Destroy(gameObject, 2);
+    }
+
+    private void OnTriggerEnter(Collider other) {
+        if (other.tag == "Platform") {
+            Destroy(gameObject);
+        }
+    }
+}
